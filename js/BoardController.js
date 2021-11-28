246a4fab67abf13e8a12b10ad16a420f8b8d14e1
klass1013 <klasseter1013@gmail.com>
2021-11-27 23:19:12 -0500

    damsphere

--- js/BoardController.js
+++ js/BoardController.js
@@ -5,59 +5,70 @@ CHECKERS.BoardController = function (options) {
 
     var containerEl = options.containerEl || null;
 
-    var assetsURL = options.assetsURL || '';
+    var assetsUrl = options.assetsUrl || '';
+
+    assetsUrl: options.assetsUrl || '';
+
+    var renderer;
+    var scene;
+    var camera;
+    var cameraController;
 
     this.drawBoard = function () {
-        console.log('drawBoard');
+        initEngine();
+        initObjects(function () {
+            onAnimationFrame();
+        });
     };
+
+    function initEngine() {
+        var viewWidth = containerEl.offsetWidth;
+        var viewHeight = containerEl.offsetHeight;
+    
+        renderer = new THREE.WebGLRenderer({
+            antialias: true
+        });
+    
+        renderer.setSize(viewWidth, viewHeight);
+    
+        scene = new THREE.Scene();
+    
+        camera = new THREE.PerspectiveCamera(35, viewWidth / viewHeight, 1, 1000);
+        camera.position.set(0,120,150);
+        cameraController = new THREE.OrbitControls(camera, containerEl);
+    
+        scene.add(camera);
+    
+        containerEl.appendChild(renderer.domElement);
+    }
+
+    function initObjects(callback) {
+        const material = new THREE.MeshBasicMaterial({
+            color: 0x00FF00,
+            wireframe: true,
+          });
+
+
+
+        var cube = new THREE.Mesh(new THREE.SphereGeometry(50, 50, 50), material);
+        //var cube = new THREE.CubeGeometry(50, 50, 50);
+        scene.add(cube);
+    
+        callback();
+    }
+    
+    function onAnimationFrame() {
+        requestAnimationFrame(onAnimationFrame);
+    
+        cameraController.update();
+        
+        renderer.render(scene, camera);
+    }
+    
 };
 
-assetsURL: options.assetsURL || '';
 
-var renderer;
-var scene;
-var camera;
-var cameraController;
 
-this.drawBoard = function () {
-    initEngine();
-    initObjects(function () {
-        onAnimationFrame();
-    });
-}
 
-function initEngine() {
-    var viewWidth = containerEl.offsetWidth;
-    var viewHeight = containerEl.offsetHeight;
 
-    renderer = new THREE.WebGLRenderer({
-        antialias: true
-    });
 
-    renderer.setSize(viewWidth, viewHeight);
-
-    scene = new THREE.Scene();
-
-    camera = new THREE.PerspectiveCamera(35, viewWidth / viewHeight, 1, 1000);
-    camera.position.set(0,120,150);
-    cameraController = new THREE.OrbitControls(camera, containerEl);
-
-    scene.add(camera);
-
-    containerEl.appendChild(renderer.domElement);
-}
-
-function initObjects(callback) {
-    var cube = new THREE.Mesh(new THREE.CubeGeometry(50, 50, 50));
-    scene.add(cube);
-
-    callback();
-}
-
-function onAnimationFrame() {
-    requestAnimationFrame(onAnimationFrame);
-
-    cameraController.update();
-    
-    renderer.render(scene, camera);
-}
