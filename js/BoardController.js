cf93dac640d5e8faaea7990daf0a4f9c76ec36b6
klass1013 <klasseter1013@gmail.com>
2021-11-29 15:17:35 -0500

    peezez

--- js/BoardController.js
+++ js/BoardController.js
@@ -21,17 +21,54 @@ CHECKERS.BoardController = function (options) {
 
     var squareSize = 10;
 
+    var board = [
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0]
+    ];
 
-    this.drawBoard = function () {
+    this.drawBoard = function (callback) {
         initEngine();
         initLights();
         initMaterials();
 
-        initObjects(function (callback) {
+        initObjects(function () {
             onAnimationFrame();
 
             callback();
         });
+    };
+
+    this.addPiece = function (piece) {
+        var pieceMesh = new THREE.Mesh(pieceGeometry);
+        var pieceObjGroup = new THREE.Object3D();
+        //
+        if (piece.color === CHECKERS.WHITE) {
+            pieceObjGroup.color = CHECKERS.WHITE;
+            pieceMesh.material = materials.whitePieceMaterial;
+        } else {
+            pieceObjGroup.color = CHECKERS.BLACK;
+            pieceMesh.material = materials.blackPieceMaterial;
+        }
+    
+        // create shadow plane
+        var shadowPlane = new THREE.Mesh(new THREE.PlaneGeometry(squareSize, squareSize, 1, 1), materials.pieceShadowPlane);
+        shadowPlane.rotation.x = -90 * Math.PI / 180;
+        shadowPlane.position.y -= 0.5;
+    
+        pieceObjGroup.add(pieceMesh);
+        pieceObjGroup.add(shadowPlane);
+    
+        pieceObjGroup.position = boardToWorld(piece.pos);
+    
+        board[ piece.pos[0] ][ piece.pos[1] ] = pieceObjGroup;
+    
+        scene.add(pieceObjGroup);
     };
 
     function initEngine() {
@@ -48,7 +85,7 @@ CHECKERS.BoardController = function (options) {
         scene = new THREE.Scene();
     
         camera = new THREE.PerspectiveCamera(35, viewWidth / viewHeight, 1, 1000);
-        camera.position.set(0,120,150);
+        camera.position.set(squareSize * 4,120,150);
         cameraController = new THREE.OrbitControls(camera, containerEl);
         cameraController.center = new THREE.Vector3(squareSize * 4, 0, squareSize * 4);
 
@@ -57,6 +94,66 @@ CHECKERS.BoardController = function (options) {
         containerEl.appendChild(renderer.domElement);
     }
 
+    
+    
+    function initLights() {
+        // top light
+        lights.topLight = new THREE.PointLight(0xFFFFFF, 1, 100000);
+        lights.topLight.position.set(squareSize * 4, 150, squareSize * 20);
+        //lights.topLight.intensity = 1.0;
+    
+        // add the lights in the scene
+        scene.add(lights.topLight);
+    }
+    
+    function initMaterials() {
+        // board material
+        
+        materials.boardMaterial = /*new THREE.MeshStandardMaterial( { color: 0x00c414 } );*/new THREE.MeshLambertMaterial({
+            map: THREE.ImageUtils.loadTexture(assetsUrl + 'board_texture.jpg')
+        });
+        
+
+       
+    
+        // ground material
+        materials.groundMaterial = new THREE.MeshBasicMaterial({
+            transparent: true,
+            map: THREE.ImageUtils.loadTexture(assetsUrl + 'ground.png')
+        });
+    
+        // dark square material
+        materials.darkSquareMaterial = new THREE.MeshLambertMaterial({
+            map: THREE.ImageUtils.loadTexture(assetsUrl + 'dark_marble_square.jpg')
+        });
+        
+        // light square material
+        materials.lightSquareMaterial = new THREE.MeshLambertMaterial({
+            map: THREE.ImageUtils.loadTexture(assetsUrl + 'light_marble_square.jpg')
+        });
+    
+        // white piece material
+        materials.whitePieceMaterial = new THREE.MeshLambertMaterial( { color: 0xffffff } );
+
+        /*new THREE.MeshPhongMaterial({
+            color: 0xe9e4bd,
+            shininess: 20
+        });*/
+    
+        // black piece material
+        materials.blackPieceMaterial = new THREE.MeshLambertMaterial( { color: 0x242424, shininess: 5 } );
+        /*new THREE.MeshPhongMaterial({
+            color: 0x9f2200,
+            shininess: 20
+        });*/
+    
+        // pieces shadow plane material
+        materials.pieceShadowPlane = new THREE.MeshBasicMaterial({
+            transparent: true,
+            map: THREE.ImageUtils.loadTexture(assetsUrl + 'piece_shadow.png')
+        });
+    }
+    
     function initObjects(callback) {
         var loader = new THREE.JSONLoader();
         var totalObjectsToLoad = 2; // board + the piece
@@ -73,74 +170,72 @@ CHECKERS.BoardController = function (options) {
         
             // load board
         loader.load(assetsUrl + 'board.js', function (geom) {
+            // load board
             boardModel = new THREE.Mesh(geom, materials.boardMaterial);
-        
+            boardModel.position.y = -0.02;
+
             scene.add(boardModel);
         
             checkLoad();
         });
-        
-            // load piece
+
+        // load piece
         loader.load(assetsUrl + 'piece.js', function (geometry) {
-            pieceGeometry = geometry;
+
+
+            const geometry2 = new THREE.CylinderGeometry(4,4,1, 100, 100);
+
+            pieceGeometry = geometry2;
         
             checkLoad();
         });
+        // add ground
+        groundModel = new THREE.Mesh(new THREE.PlaneGeometry(100, 100, 1, 1), materials.groundMaterial);
+        groundModel.position.set(squareSize * 4, -1.52, squareSize * 4);
+        groundModel.rotation.x = -90 * Math.PI / 180;
+        //
+        scene.add(groundModel);
+
+        // create the board squares
+        var squareMaterial;
+        //
+        for (var row = 0; row < 8; row++) {
+            for (var col = 0; col < 8; col++) {
+                if ((row + col) % 2 === 0) { // light square
+                    squareMaterial = materials.lightSquareMaterial;
+                } else { // dark square
+                    squareMaterial = materials.darkSquareMaterial;
+                }
+
+                var square = new THREE.Mesh(new THREE.PlaneGeometry(squareSize, squareSize, 1, 1), squareMaterial);
+
+                square.position.x = col * squareSize + squareSize / 2;
+                square.position.z = row * squareSize + squareSize / 2;
+                square.position.y = -0.01;
+
+                square.rotation.x = -90 * Math.PI / 180;
+
+                var random = Math.random();
+                if(random < .25){
+                    square.rotation.z = 90 * Math.PI / 180;
+                }else if(.25 <= random < .50){
+                    square.rotation.z = 180 * Math.PI / 180;
+                }else if(.50 <= random < .75){
+                    square.rotation.z = 270 * Math.PI / 180;
+                }
+
+                scene.add(square);
+            }
+        }
+
+
+            
+        
+        
+        
         
         scene.add(new THREE.AxisHelper(200));    
     }
-    
-    function initLights() {
-        // top light
-        lights.topLight = new THREE.PointLight();
-        lights.topLight.position.set(0, 150, 0);
-        lights.topLight.intensity = 1.0;
-    
-        // add the lights in the scene
-        scene.add(lights.topLight);
-    }
-    
-    function initMaterials() {
-        // board material
-        materials.boardMaterial = new THREE.MeshLambertMaterial({
-            map: THREE.ImageUtils.loadTexture(assetsUrl + 'board_texture.jpg')
-        });
-    
-        // ground material
-        materials.groundMaterial = new THREE.MeshBasicMaterial({
-            transparent: true,
-            map: THREE.ImageUtils.loadTexture(assetsUrl + 'ground.png')
-        });
-    
-        // dark square material
-        materials.darkSquareMaterial = new THREE.MeshLambertMaterial({
-            map: THREE.ImageUtils.loadTexture(assetsUrl + 'square_dark_texture.jpg')
-        });
-        //
-        // light square material
-        materials.lightSquareMaterial = new THREE.MeshLambertMaterial({
-            map: THREE.ImageUtils.loadTexture(assetsUrl + 'square_light_texture.jpg')
-        });
-    
-        // white piece material
-        materials.whitePieceMaterial = new THREE.MeshPhongMaterial({
-            color: 0xe9e4bd,
-            shininess: 20
-        });
-    
-        // black piece material
-        materials.blackPieceMaterial = new THREE.MeshPhongMaterial({
-            color: 0x9f2200,
-            shininess: 20
-        });
-    
-        // pieces shadow plane material
-        materials.pieceShadowPlane = new THREE.MeshBasicMaterial({
-            transparent: true,
-            map: THREE.ImageUtils.loadTexture(assetsUrl + 'piece_shadow.png')
-        });
-    }
-    
 
     function onAnimationFrame() {
         requestAnimationFrame(onAnimationFrame);
@@ -150,6 +245,12 @@ CHECKERS.BoardController = function (options) {
         renderer.render(scene, camera);
     }
     
+    function boardToWorld (pos) {
+        var x = (1 + pos[1]) * squareSize - squareSize / 2;
+        var z = (1 + pos[0]) * squareSize - squareSize / 2;
+    
+        return new THREE.Vector3(x, .5, z);
+    }    
 };
 
 
--- js/Game.js
+++ js/Game.js
@@ -1,4 +1,7 @@
-var CHECKERS = {};
+var CHECKERS = {
+    WHITE: 1,
+    BLACK: 2
+};
 
 CHECKERS.Game = function (options) {
     'use strict';
@@ -7,6 +10,16 @@ CHECKERS.Game = function (options) {
 
     var boardController = null;
 
+    var board = [
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0]
+    ];
 
     function init() {
         boardController = new CHECKERS.BoardController({
@@ -14,10 +27,38 @@ CHECKERS.Game = function (options) {
             assetsUrl: options.assetsUrl
         });
 
-        boardController.drawBoard();
+        boardController.drawBoard(onBoardReady);
     }
 
+    function onBoardReady() {
+        var row, col, piece;
 
+        for(row = 0; row < board.length; row++){
+            for(col = 0; col < board[row].length; col++){
+                if( (row +col )% 2){
+                    if(row < 3){
+                        piece = {
+                            color: CHECKERS.BLACK,
+                            pos: [row, col]
+                        };
+                    }else if(row > 4){
+                        piece = {
+                            color: CHECKERS.WHITE,
+                            pos: [row, col]
+                        };
+                    }else{
+                        piece = 0;
+                    }
+                }   
+
+                board[row][col] = piece;
+
+                if(piece){
+                    boardController.addPiece(piece);
+                }
+            }
+        }
+    }
 
     init();
 };
